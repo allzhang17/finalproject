85e7620e473394a927ade357b2e71e36e1ed673d
Allison <allzhang17@gmail.com>
2020-04-26 13:45:28 -0500

    very rough UI draft

--- app/src/main/java/com/example/turnipcalc/EnterInfoActivity.java
+++ app/src/main/java/com/example/turnipcalc/EnterInfoActivity.java
@@ -3,10 +3,16 @@ package com.example.turnipcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class EnterInfoActivity extends AppCompatActivity {
+
+    CheckBox am;
+    CheckBox pm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,29 @@ public class EnterInfoActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.select));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         daySpinner.setAdapter(myAdapter);
+
+        am = findViewById(R.id.amCheck);
+        pm = findViewById(R.id.pmCheck);
+
+        am.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (am.isChecked()) {
+                    Toast.makeText(EnterInfoActivity.this, "8 a.m. to 11:59 a.m.",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+        pm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pm.isChecked()) {
+                    Toast.makeText(EnterInfoActivity.this, "12 p.m. to 10 p.m.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
