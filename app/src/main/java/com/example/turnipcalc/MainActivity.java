2db802c9c4f08705951113d288fb741fbedcc095
Allison <allzhang17@gmail.com>
2020-04-27 16:30:42 -0500

    troubleshoooot

--- app/src/main/java/com/example/turnipcalc/MainActivity.java
+++ app/src/main/java/com/example/turnipcalc/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.turnipcalc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button infoButton = findViewById(R.id.infoButton);
+        Button infoButton = (Button) findViewById(R.id.infoButton);
         infoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
